77896b948823dfa483882ea676da0cf300d193f9
THender613 <116307058+THender613@users.noreply.github.com>
2025-01-31 17:00:23 +0000

    Finished data shapes

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -68,7 +68,11 @@ $(document).ready(function () {
   }
 
   // TODO 5-a: add a function that handles the bad display type
-  
+  function handleBad(data, repeat) {
+    repeat = repeat + 1; 
+    setBackgroundWithMixed(data, repeat); 
+    animationDetails.displayType = 3; 
+  }
 
   /////////////////////////////////////////////////
   // BUTTON HANDLERS BELOW HERE (3-b, 4-b, 5-b) ///
@@ -81,12 +85,15 @@ $(document).ready(function () {
 
   function goodDisplay() {
     // TODO 4-b: call your handleGood function
-    var currentShape()
+    var currentShape = dataShapes[currentIndex]; 
+    handleGood(currentShape.color, currentShape.shape, currentShape.repeat);
   }
 
   function badDisplay() {
     // TODO 5-b: call your handleBad function
-    
+    var currentShape = dataShapes[currentIndex];
+    var repeat = currentShape.repeat; 
+    handleBad(currentShape, repeat); 
   }
 
   /////////////////////////////////////////////////
